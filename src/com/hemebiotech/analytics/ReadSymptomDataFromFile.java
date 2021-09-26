6033e90522bdcbbecce82132a7a76c93dae25f29
faical nachiti <nachiti3@gmail.com>
2021-09-26 22:50:39 +0200

    premier commit

--- src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -3,8 +3,8 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Simple brute force implementation
@@ -13,7 +13,7 @@ import java.util.List;
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
 	private String filepath;
-	
+	Map<String, Integer> list_Symptoms = new HashMap<String, Integer>();
 	/**
 	 * 
 	 * @param filepath a full or partial path to file with symptom strings in it, one per line
@@ -23,25 +23,38 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
 	}
 	
 	@Override
-	public List<String> GetSymptoms() {
-		ArrayList<String> result = new ArrayList<String>();
+	public Map<String, Integer> getSymptoms() {
 		
 		if (filepath != null) {
 			try {
 				BufferedReader reader = new BufferedReader (new FileReader(filepath));
-				String line = reader.readLine();
+				String line;
 				
-				while (line != null) {
-					result.add(line);
-					line = reader.readLine();
-				}
+				while ((line=reader.readLine()) != null) {
+	
+						if(searchSymptomsInMap(line)) {
+							list_Symptoms.put(line, list_Symptoms.get(line)+1);
+							//System.out.println("Symptom----- : "+line+ " | number------ : " + list_Symptoms.get(line));
+						}else {
+							list_Symptoms.put(line, 1);
+						}
+					}
+				
+
 				reader.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		}else {
+			System.out.println("The file does not exist.");
 		}
 		
-		return result;
+		return list_Symptoms;
+	}
+	
+	private boolean searchSymptomsInMap(String line) {
+		return list_Symptoms.containsKey(line)?true:false;
+	
 	}
 
 }
